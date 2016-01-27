078d2bd756cb28248f7dc5b160dcb70f4169a365
thiaSSK <thia@sfc.wide.ad.jp>
2016-01-27 12:45:36 +0900

    Create SSample1_2.java

---
+++ SSample1_2.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+ 
+class SSample1_2{
+public static void main(String args[]){
+JFrame frame = new JFrame("タイトル");
+  frame.setBounds(100, 100, 200, 160);
+ frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ frame.setVisible(true);
+  }
+ }
