993742c5d019642e7371e5e0d85f2765194f07ea
rokuden <rokuden@sfc.wide.ad.jp>
2016-01-27 16:25:02 +0900

    Create SwingTest.java

---
+++ SwingTest.java
@@ -0,0 +1,96 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TreeSet;
+
+public class SwingTest extends JFrame{
+
+	/*  public static void main(String[] args){
+            //ディレクトリ指定
+            String path = "/Users/Rokuden/Documents";
+            File dir = new File(path);
+            
+            //フルパスで取得
+            System.out.println("--ファイル一覧(フルパス)--");
+            File[] files1 = dir.listFiles();
+            for (int i = 0; i < files1.length; i++) {
+               File file = files1[i];
+               if (files1[i].isFile()){
+                   //ファイル名表示
+                    System.out.println(file);
+               }else if (files1[i].isDirectory()){
+                   //ディレクトリ名表示(※１)
+                   System.out.println(file);
+                }
+            }
+        }
+}*/
+
+
+  private String[][] tabledata = {
+    {"日本", "3勝", "0敗", "1分"},
+    {"クロアチア", "3勝", "1敗", "0分"},
+    {"ブラジル", "1勝", "2敗", "1分"},
+    {"オーストラリア", "2勝", "2敗", "0分"}};
+
+  private String[] columnNames = {"COUNTRY", "WIN", "LOST", "EVEN"};
+
+  public static void main(String[] args){
+    SwingTest test = new SwingTest("SwingTest");
+
+    test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    test.setVisible(true);
+  }
+  
+
+  SwingTest(String title){
+    setTitle(title);
+    setBounds( 10, 10, 300, 200);
+    
+    String path = "/Users/Rokuden/Documents";
+    File dir = new File(path);
+    
+    //フルパスで取得
+    //System.out.println("--ファイル一覧(フルパス)--");
+
+    File[] files1 = dir.listFiles();
+    
+    Object[][] contents = new Object[][];
+    for (int i = 0; i < files1.length; i++) {
+    	/*
+       File file = files1[i];
+       if (files1[i].isFile()){
+           //ファイル名表示
+            System.out.println(file);
+       }else if (files1[i].isDirectory()){
+           //ディレクトリ名表示(※１)
+           System.out.println(file);
+        }
+        */
+    	contents[i] = new TableContents(files1[i], i);
+    }
+    JTable table = new JTable(contents, columnNames);
+
+    JScrollPane sp = new JScrollPane(table);
+    sp.setPreferredSize(new Dimension(250, 70));
+
+    JPanel p = new JPanel();
+    p.add(sp);
+
+    getContentPane().add(p, BorderLayout.CENTER);
+  }
+}
+
+class TableContents {
+	public File file;
+	public int id;
+	
+	TableContents(File f, int i) {
+		file = f;
+		id = i;
+	}
+}
